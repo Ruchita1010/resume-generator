53f976a4ed0850f95b5a082d2225d99d56343d8f
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-03-07 21:20:14 +0530

    feat: add layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import './styles/app.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
+    <div className="app">
+      <header>
+        <div className="logo">cv generator</div>
+      </header>
+      <main>
+        <div className="progressive-form-container">
+          <h1>Progress bar</h1>
+          <h1>Form</h1>
+        </div>
+        <div className="preview">
+          <h1>Preview</h1>
+        </div>
+      </main>
     </div>
   );
 }
