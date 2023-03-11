9f2199563ce155b0a6956ebd7dc0c1eba072f341
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-03-11 22:18:34 +0530

    feat: add step Skills

--- src/components/Steps/Skills.jsx
+++ src/components/Steps/Skills.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import FormElement from '../FormElement';
+import styles from '../../styles/stepUtils.module.css';
 
 const Skills = () => {
-  return <>Input Skills</>;
+  return (
+    <div className={styles.input_section}>
+      <FormElement
+        label="Skills"
+        type="text"
+        name="skills"
+        placeholder="e.g. React"
+      />
+      <button className={`${styles.btn} ${styles.add_skills_btn}`}>+</button>
+    </div>
+  );
 };
 
 export default Skills;
