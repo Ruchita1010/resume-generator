c2f276b7507aaa65bc6d5597c531c3ae4e3f3579
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-03-11 20:32:38 +0530

    feat: add step Projects

--- src/components/Steps/Projects.jsx
+++ src/components/Steps/Projects.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import FormElement from '../FormElement';
+import styles from '../../styles/stepUtils.module.css';
 
 const Projects = () => {
-  return <>Input Projects</>;
+  return (
+    <>
+      <div className={styles.add_btn_container}>
+        <button>Add More</button>
+      </div>
+      <div className={`${styles.input_section} ${styles.card}`}>
+        <FormElement
+          label="Project Name"
+          type="text"
+          name="project-name"
+          placeholder="e.g. E-commerce Website"
+        />
+        <FormElement
+          label="Technologies Used"
+          type="text"
+          name="technologies-used"
+          placeholder="e.g. React, Firebase, Git"
+        />
+        <div className={styles.description} id={styles.project_desc_container}>
+          <label htmlFor="project-desc">Description</label>
+          <textarea
+            id="project-desc"
+            placeholder="e.g. Implemented a secure payment gateway using Stripe API&#10;Integrated Firebase for user authentication"></textarea>
+        </div>
+        <button>Delete</button>
+      </div>
+    </>
+  );
 };
 
 export default Projects;
