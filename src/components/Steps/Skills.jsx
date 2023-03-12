9dc6bd9576f3506191fb181ab32c6ca934628116
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-03-13 00:01:10 +0530

    feat: change layout of step Skills Made it's layout consistent with other steps in turn making it easier to update the state.

--- src/components/Steps/Skills.jsx
+++ src/components/Steps/Skills.jsx
@@ -4,17 +4,21 @@ import styles from '../../styles/stepStyles.module.css';
 
 const Skills = () => {
   return (
-    <div className={styles.input_section}>
-      <FormElement
-        formAttr={{
-          type: 'text',
-          name: 'skills',
-          placeholder: 'e.g. React',
-        }}
-        otherData={{ label: 'Skills', category: 'skills' }}
-      />
-      <button className={`${styles.btn} ${styles.add_skills_btn}`}>+</button>
-    </div>
+    <>
+      <div className={styles.add_btn_container}>
+        <button>Add More</button>
+      </div>
+      <div className={styles.input_section}>
+        <FormElement
+          formAttr={{
+            type: 'text',
+            name: 'skills',
+            placeholder: 'e.g. React',
+          }}
+          otherData={{ label: 'Skill', category: 'skills' }}
+        />
+      </div>
+    </>
   );
 };
 
