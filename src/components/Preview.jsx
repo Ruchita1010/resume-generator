035000364f40ba9fdd5435c1d960cca50a0f39c8
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-03-14 15:04:03 +0530

    feat: add preview of Skills

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import PersonalDetails from './Previews/PersonalDetails';
 import Education from './Previews/Education';
 import styles from '../styles/Preview.module.css';
+import Skills from './Previews/Skills';
 
 const Preview = (props) => {
-  const { personalDetails, education } = props.user;
+  const { personalDetails, education, skills } = props.user;
   return (
     <div className={styles.preview}>
       <PersonalDetails personalDetails={personalDetails} />
       <Education education={education} />
+      <Skills skills={skills} />
     </div>
   );
 };
